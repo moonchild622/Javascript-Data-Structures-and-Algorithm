3523ddc59045e2902bdae25d754746a4f5570fd1
Muhammad Sani Haruna <muhammadsaniharuna44@gmail.com>
2022-08-19 15:11:52 +0100

    fixes

--- Arrays.js
+++ Arrays.js
@@ -119,3 +119,22 @@ var twoSmArray = function(nums, target){
 
 console.log(twoSmArray([10,30,40,50,60,70,80,90], 170));
 
+
+//Leetcode question 2 
+const removeDuplicates = nums => {
+    
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] !== nums[i + 1] && nums[i + 1] != undefined){
+            index++;
+            nums[index - 1] = nums[i];
+            nums[index] = nums[i + 1];
+        } 
+    }
+    return index + 1;
+    
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+
+
